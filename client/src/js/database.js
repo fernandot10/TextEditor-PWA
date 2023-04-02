a068a48d7080268341d61059cfa955b8cec62591
fernandot10 <113951371+fernandot10@users.noreply.github.com>
2023-04-02 15:25:15 -0400

    client-js / database & install fixed

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,6 @@
 import { openDB } from 'idb';
 
+
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -12,10 +13,27 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  console.log('Updating the database...');
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, content: content });
+  const result = await request;
+  console.log('Database successfully updated!', result);
+}
+
+
+export const getDb = async () => {
+  console.log('Retrieving data from the database...');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log('Retrieved data from database!', result);
+  return result.content;
+}
 
 initdb();
